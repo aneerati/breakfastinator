a9212a62ad417441a9e2e41435494676319c5519
aneerati <aneerati2004@gmail.com>
2025-05-14 10:54:17 -0400

    finished it

--- src/main.c
+++ src/main.c
@@ -2,5 +2,26 @@
 
 int main()
 {
-    printf("Hello World\n");
+    int input;
+    int score = 0;
+    printf("First meal of Day?\n\t[1] YES\t[0] NO: ");
+    scanf("%d", &input);
+    score += input;
+
+    printf("\nBreakfast Food?\n\t[1] YES\t[0] NO: ");
+    scanf("%d", &input);
+    score += input;
+
+    printf("\nBreakfast Time?\n\t[1] YES\t[0] NO: ");
+    scanf("%d", &input);
+    score += input;
+
+    if (score >= 2)
+    {
+        printf("\nBREAKFAST!\n");
+    }
+    else
+    {
+        printf("\nSHIT AIN'T BREAKFAST!\n");
+    }
 }
